c8c8b0486bf8c700fe89eef250d92dee66536670
evinanil2 <123241343+evinanil2@users.noreply.github.com>
2023-05-13 20:11:22 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+<script>
+console.log("Hi I am Console Screen");
+</script>
+var first_name="EVIN";
+var last_name="Anil";
+console.log (last_name);
+first_name.concat(last_name);
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var length_of_full_name=full_name.length;
+console.log ;
+
+var a = 12;
+var b = 13;
+
+var add = a + b
+console.log (add);
+
+
+
+
